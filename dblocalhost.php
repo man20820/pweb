5e4b4ea52f13cd63d492cec22a2931f2b92d0603
man20820 <fatkhurrahmandjogja@gmail.com>
2022-05-12 21:50:33 +0700

    modify jenkinsfile

--- dblocalhost.php
+++ dblocalhost.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "pweb_uts";
+$dbname = "ppweb_uts";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
